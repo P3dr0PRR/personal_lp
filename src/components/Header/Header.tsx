3cb0c8bde064ec8851a99c38ff43123bc07ca661
Pedro Paulo <pedropaulo@interlinksistemas.com.br>
2026-05-11 21:10:35 -0300

    Home finished

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,24 @@
-import Jhon from "../assets/JhonDoe.png";
-import Menu from "../assets/menu.png";
+import Jhon from "../../assets/JhonDoe.png";
+import Menu from "../../assets/menubg.png";
 import { useState } from "react";
 
 export function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const logo = (
-    <img src={Jhon} alt="Jhon Doe" className="h-22 object-contain" />
+    <img
+      src={Jhon}
+      alt="Jhon Doe"
+      className="h-22 object-contain bg-transparent"
+    />
   );
   const links = (
     <>
-      <a href="#" className="NavBTNS">
+      <a href="#Home" className="NavBTNS">
         Home
       </a>
-      <a href="#" className="NavBTNS">
-        Portifolio
+      <a href="#Portfolio" className="NavBTNS">
+        Portfolio
       </a>
     </>
   );
@@ -33,18 +37,18 @@ export function Header() {
               onClick={() => setIsMenuOpen(!isMenuOpen)}
             />
           </div>
-          <div className="hidden md:flex items-center p-4 gap-4 h-full w-full justify-end">
+          <nav className="hidden md:flex items-center p-4 gap-4 h-full w-full justify-end">
             {links}
-          </div>
+          </nav>
         </div>
       )}
 
       {isMenuOpen && (
         <div className="flex flex-col md:flex-row justify-center items-center md:justify-between p-4 pt-6 w-full">
           {logo}
-          <div className="flex items-center justify-center md:justify-end gap-4 h-full w-full p-4">
+          <nav className="flex items-center justify-center md:justify-end gap-4 h-full w-full p-4">
             {links}
-          </div>
+          </nav>
         </div>
       )}
     </header>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,48 @@
+import Menubg from "../assets/personal.jpg";
+import JDoe from "../assets/Jdoe.png";
+import Wpp from "../assets/wpp.png";
+
+import { Footer } from "../components/Header/Footer";
+
+export function Home() {
+  return (
+    <>
+      <section
+        id="Home"
+        className="min-h-screen w-full flex items-center justify-center relative"
+      >
+        <img
+          src={Menubg}
+          alt="Personal"
+       className="absolute inset-0 h-full w-full object-cover"
+        />
+       <div className="relative z-10 flex flex-col min-h-[25vh] items-center justify-center gap-4 text-center py-32">
+          <div className="flex flex-col items-center justify-center gap-4 text-center ">
+            <img
+              src={JDoe}
+              alt="Jon Doe J"
+              className="h-32 w-32 bg-transparent"
+            />
+            <h2>Com foco e direção, você vai além do que imagina.</h2>
+            <p className="motivation">buscando direção ?</p>
+            <h2>Entre em Contato</h2>
+            <p className="motivation">
+              Do primeiro treino ao resultado final, eu cuido de cada detalhe.
+              Seu único trabalho é evoluir.
+            </p>
+            <button className="BTNSlinks flex items-center gap-2 bg-green-500 hover:bg-green-600 cursor-pointer">
+              <img src={Wpp} alt="WhatsApp" className="h-6 w-6" />
+              Atendimento via WhatsApp
+            </button>
+            <span className="h-0.5 px-[25%] bg-indigo-50"></span>
+            <div className="flex flex-col items-center justify-center gap-4 text-center ">
+              <h2>Acesse nosso Portfolio</h2>
+              <button className="BTNSlinks">Portfolio</button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+}
